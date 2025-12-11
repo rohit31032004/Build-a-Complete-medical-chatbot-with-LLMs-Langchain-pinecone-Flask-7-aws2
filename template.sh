77d6e7003a92d441b2bb80e1447f0a2c117e0510
Rohit Kumar Shukla <204577125+rohit31032004@users.noreply.github.com>
2025-12-11 16:20:29 +0530

    folder structure added
    git push origin main --force# Please enter the commit message for your changes. Lines starting

---
+++ template.sh
@@ -0,0 +1,16 @@
+#Creating directorie
+mkdir -p src
+mkdir -p research
+
+#Creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/promt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirement.txt
+
+
+echo "Directory and files created successfully!."
